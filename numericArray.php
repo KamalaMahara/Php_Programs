4113f4d0e56ff43368e392788186f7f5d3028907
kamala mahara <kmlamahara@gmail.com>
2025-07-30 15:27:18 +0545

    numeric index array

--- numericArray.php
+++ numericArray.php
@@ -1,6 +1,13 @@
 <?php
 //numeric index array
 $animal = array("cat", "dog", "elephant", "tiger", "wolf");
+
+// array functions
+array_push($animal, "goat", "cow");   // adding elements to the end of the array
+array_pop($animal);     //removes the last element in the array
+array_shift($animal);    //removes the first element in the aaray
+array_unshift($animal, "meowww"); //adds an element to the beginning of the array
+
 echo $animal[0] . "<br>";
 for ($i = 1; $i < count($animal); $i++) {
   echo $animal[$i] . "<br>";
