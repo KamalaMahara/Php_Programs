d38063a05c9c08e0b8e4cf6f01cd99777abb6ea2
kamala mahara <kmlamahara@gmail.com>
2025-08-06 22:59:24 +0545

    validaations in php

---
+++ validation.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="validation.php" method="post">
+    Username:
+    <input type="text" name="name" id="name"><br>
+
+    Age:
+    <input type="text" name="age" id="age"><br>
+    email:
+    <input type="email" name="email" id="email"><br>
+    <input type="submit" value="login" name="login"><br>
+
+  </form>
+</body>
+
+</html>
+<?php
+// validation in php
+if (isset($_POST["login"])) {
+  $age = filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT);
+  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+  if (empty($age)) {
+    echo "please enter a valid age";
+  } else {
+    echo "your age is: {$age} <br>";
+  }
+  echo "<BR>";
+  if (empty($email)) {
+    echo "please enter a valid email";
+  } else {
+    echo "your email is: {$email} <br>";
+  }
+}
