dd97d458175c983b48e4dbe33a6cf887d2219c7e
kamala mahara <kmlamahara@gmail.com>
2025-08-09 11:18:27 +0545

    hashing in php

--- hashing.php
+++ hashing.php
@@ -1,8 +1,8 @@
 <?php
 //hashing= transforming sensitive data (password) into letters,numbers, or symbols via a hashing algorithm(similar to encryption ,hides the original data from 3rd parties)
-$password = "@!13km";
-$hash = password_hash($password, PASSWORD_DEFAULT); // hashing the password using the default algorithm
-if (password_verify($password, $hash))  // verifying the password agaiinst the hash
+$pass = "@!13km";
+$hash = password_hash($pass, PASSWORD_DEFAULT); // hashing the password using the default algorithm
+if (password_verify($pass, $hash))  // verifying the password agaiinst the hash
 {
   echo "logged in successfully";
 } else {
